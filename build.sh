df8ef29ca218280cf24603c12fd34123c836b458
chickencuber <n1batman728@gmail.com>
2024-12-03 22:07:32 -0600

    updated thing

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ i686-elf-gcc -ffreestanding -m32 -g -c "./src/kernel.c" -o "./bin/kernel.o"
 nasm ./src/kernel_entry.asm -f elf -o ./bin/kernel_entry.o
 i686-elf-ld -o bin/kernel.bin -Ttext 0x1000 bin/kernel_entry.o bin/kernel.o --oformat binary
 nasm ./src/boot.asm -f bin -o ./bin/boot.bin
-cat ./bin/boot.bin ./bin/kernel.bin > ./bin/os.bin
+nasm -f bin ./src/zero.asm -o ./bin/zero.bin
+cat ./bin/boot.bin ./bin/kernel.bin ./bin/zero.bin > ./bin/os.bin

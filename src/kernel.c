d32851e0d28ea163678da6677dd8d053a0cec7bc
chickencuber <n1batman728@gmail.com>
2024-12-20 20:04:29 -0600

    fixed malloc

--- src/kernel.c
+++ src/kernel.c
@@ -1,10 +1,12 @@
-#define HEAP_START 0x100000   // Start of heap at 1MB
 #define HEAP_SIZE  0x100000   // 1MB heap size
 #define true 1
 #define false 0
 
 typedef char bool;
 typedef unsigned long uint;
+
+char HEAP_START[HEAP_SIZE]; // the heap, its a char because chars are always 1 byte
+
 
 //io
 static struct {
@@ -56,147 +58,222 @@ extern void error(char* arr) {
     io.color = color;
 }
 
-struct Chunk {
-    void* ptr;
-    uint size;
+//allocator
+typedef struct {
+    uint size;    
     bool free;
-    struct Chunk* next; 
-    struct Chunk* last;
-};
+    void* ptr;
+} Chunk;
 
-typedef struct Chunk Chunk;
+struct {
+    Chunk chunks[HEAP_SIZE];
+    uint size;
+    Chunk (*remove)(uint);
+    void (*insert)(Chunk, uint);
+} Chunks = {{}, 0};
 
-static struct {
-    Chunk chunk; 
-    uint size; 
-    void (*fix_malloc)(Chunk*, uint);
-    void (*fix_free)(Chunk*);
-} Allocator;
+Chunk _remove(uint i) {
+    if (i >= Chunks.size) {
+        error("Invalid index\0");
+        Chunk t;
+        return t;
+    }
+    Chunk temp = Chunks.chunks[i];
+    for (uint j = i; j < Chunks.size - 1; j++) {
+        Chunks.chunks[j] = Chunks.chunks[j + 1]; 
+    }
+    Chunks.size--;
+    return temp;
+}
 
-void __fix_malloc__(Chunk* chunk, uint size) {
-    if(chunk->size == size) {
+void _insert(Chunk chunk, uint i) {
+    if (Chunks.size >= HEAP_SIZE) {
+        error("Too Many Chunks\0");
         return;
     }
-    Chunk c = {
-        chunk->ptr + size,
-        chunk->size - size,   
-        true,
-        chunk->next,
-        chunk,
-    };
-    chunk->size = size;
-    chunk->next = &c;
-    Allocator.size++;
+    for (uint j = Chunks.size; j > i; j--) {
+        Chunks.chunks[j] = Chunks.chunks[j - 1]; 
+    }
+    Chunks.chunks[i] = chunk;
+    Chunks.size++;
 }
 
-void __fix_free__(Chunk* chunk) {
-    if(chunk->next) {
-        if(chunk->next->free) {
-            chunk->size += chunk->next->size;
-            if(chunk->next->next) {
-                chunk->next->next->last = chunk;
-            }
-            chunk->next = chunk->next->next;
-            Allocator.size--;
-        } 
-    }
-    if(chunk->last) { 
-        if(chunk->last->free) {
-            __fix_free__(chunk->last);
-        }
-    }
-}
 
 void def_Allocator() {
-    Chunk chunk = {
-        (void*) HEAP_START,
+    Chunks.remove = &_remove; 
+    Chunks.insert = &_insert;
+    Chunk c = {
         HEAP_SIZE,
         true,
+        &HEAP_START,
     };
-    Allocator.chunk = chunk;
-    Allocator.size = 1;
-    Allocator.fix_malloc = &__fix_malloc__;
-    Allocator.fix_free = &__fix_free__;
+    Chunks.insert(c, 0);
 }
 
-extern void* malloc(uint size) {
+void* malloc(uint size) {
+    Chunk* chunk;
     bool init = false;
-    void* ptr;
-    Chunk* chunk = &Allocator.chunk;
-    for(uint i = 0; i < Allocator.size; i++) {
+    uint i = 0;
+    for(i = 0; i < Chunks.size; i++) {
+        chunk = &Chunks.chunks[i]; 
         if(chunk->free && chunk->size >= size) {
-            chunk->free = false;
-            ptr = chunk->ptr;
             init = true;
             break;
         }
-        chunk = chunk->next;
     }
     if(!init) {
-        error("Alloc Error: out of memory\0");          
-        return false;
+        error("Alloc Error: Out of memory\0");
+        return 0;
     }
-    Allocator.fix_malloc(chunk, size);
-    return ptr;
+    if(chunk -> size > size) {
+        Chunk c = {
+           chunk -> size - size,
+           true,
+           chunk->ptr + size,
+        };
+        chunk->size = size;
+        Chunks.insert(c, i+1);
+    }
+    chunk->free = false;
+    return chunk->ptr;
 }
 
-extern void free(void* ptr) {
+typedef struct {
+    uint index;
+    Chunk* chunk;
+} ChunkI;
+
+ChunkI get_chunk(void* ptr) {
+    uint i = 0;
     bool init = false;
-    Chunk* chunk = &Allocator.chunk;
-    for(uint i = 0; i < Allocator.size; i++) {
-        if(chunk->ptr == ptr) {
-            if(chunk->free) {
-                error("Alloc Error: ptr not found\0");
-                return;
-            }
-            init = true;
-            break;
-        }
-        chunk = chunk->next;
+    Chunk* chunk;
+    for(i = 0; i < Chunks.size; i++) {
+       chunk = &Chunks.chunks[i];
+       if(chunk->ptr == ptr) {
+           init = true;
+           break;
+       }
     }
-    if(!init) {
-        error("Alloc Error: ptr not found\0");          
-        return;
+    if(chunk->free || !init) {
+        error("Alloc Error: ptr not found\0");
+        ChunkI c;
+        return c;
+    }
+    ChunkI c = {
+        i,
+        chunk,
+    };
+    return c;
+}
+
+void free(void* ptr) {
+    ChunkI temp = get_chunk(ptr);
+    Chunk* chunk = temp.chunk;
+    uint i = temp.index;
+    chunk->free = true;
+    if(i < Chunks.size - 1) {
+        Chunk* next = &Chunks.chunks[i+1];
+        if(next->free) {
+            chunk->size += next->size;
+            Chunks.remove(i + 1);
+        }
+    }
+    if(i > 0) {
+        Chunk* pre = &Chunks.chunks[i-1];
+        if(pre->free) {
+            chunk->size += pre->size;
+            chunk->ptr = pre->ptr;
+            Chunks.remove(i-1);
+        }
+    }
+}
+
+void* realloc(void* ptr, uint size) {
+    ChunkI temp = get_chunk(ptr);
+    Chunk* chunk = temp.chunk;
+    uint i = temp.index;
+    if(size < chunk->size) {
+        if(i < Chunks.size - 1) {
+            Chunk* next = &Chunks.chunks[i+1];
+            if(next->free) {
+                next->size += chunk->size - size;
+                next->ptr -= chunk->size - size;
+                chunk->size = size;
+                return ptr;
+            } 
+        }
+        Chunk c = {
+            chunk->size - size,
+            true,
+            chunk->ptr + size,
+        };
+        Chunks.insert(c, i + 1);
+        chunk->size = size;
+        return ptr;
+    }
+    if(size == chunk->size) {
+        return ptr;
+    }
+    if(i < Chunks.size - 1) {
+        Chunk* next = &Chunks.chunks[i+1];
+        if(next->free && next->size > size - chunk->size) {
+            next->size -= size - chunk->size; 
+            next->ptr += size - chunk->size;
+            chunk->size = size;
+            return ptr;
+        } else if(next->free && next->size == size - chunk->size) {
+            Chunks.remove(i+1);
+            chunk->size = size;
+            return ptr;
+        }
+    }
+    char* t = malloc(size);
+    for(uint i = 0; i < chunk->size; i++) {
+       t[i] = ((char*) ptr)[i]; 
     }
     chunk->free = true;
-    Allocator.fix_free(chunk);
-}
-
-extern void* realloc(void* ptr, uint size) {
-    bool init = false;
-    Chunk* chunk = &Allocator.chunk;
-    for(uint i = 0; i < Allocator.size; i++) {
-        if(chunk->ptr == ptr) {
-            if(chunk->free) {
-                error("Alloc Error: ptr not found\0");
-                return 0;
-            }
-            init = true;
-            break;
+    if(i < Chunks.size - 1) {
+        Chunk* next = &Chunks.chunks[i+1];
+        if(next->free) {
+            chunk->size += next->size;
+            Chunks.remove(i + 1);
         }
-        chunk = chunk->next;
-    }    
-    char* new = malloc(size);
-    uint _size = chunk->size;
-    for(uint i = 0; i < (size < _size ? size : _size); i++) {
-       new[i] = ((char*) ptr)[i]; 
     }
-    free(ptr);
-    return new;
+    if(i > 0) {
+        Chunk* pre = &Chunks.chunks[i-1];
+        if(pre->free) {
+            chunk->size += pre->size;
+            chunk->ptr = pre->ptr;
+            Chunks.remove(i-1);
+        }
+    }
+    return t;
 }
 
+//test
 void test() {
-    char* c = malloc(2);    
-    c[0] = '!'; 
-    c[1] = '\0';
-    print("Hello World\0");
+    char* c = malloc(sizeof(char) * 13);
+    c[0] = 'H';
+    c[1] = 'e';
+    c[2] = 'l';
+    c[3] = 'l';
+    c[4] = 'o';
+    c[5] = ' ';
+    c[6] = 'w';
+    c[7] = 'o';
+    c[8] = 'r';
+    c[9] = 'l';
+    c[10] = 'd';
+    c[11] = '!';
+    c[12] = '\0';
     println(c);
     free(c);
 }
 
 extern void main() {
     //initializes defaults
-    def_Allocator();
     io.color = 0x0f;
+    def_Allocator();
     test();
 }
+
